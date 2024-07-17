5702b32f324e5c744fc66e1bbaa3a298ab33e82c
Eduard199836 <ecamas2015@hotmail.com>
2024-07-17 13:40:32 -0500

    Primer commit

---
+++ search.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    
+    
+    
+    <!-- font-awesome font -->
+    <link rel="stylesheet" href="font/font-awesome.css" type="text/css" media="screen">
+    <!-- font-awesome font end -->
+    
+    <script src="js/jquery.js"></script>
+    <script src="search/search.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header id="header" class="header2">
+      <div class="menuheader">
+          <div class="container">
+            <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="Dock" src="img/logomain.png"></a></h1>
+            <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                <ul class="nav sf-menu">
+                  <li><a href="index.html">home</a></li>
+                  <li><a href="index-1.html">about</a>
+                    <ul>
+                      <li><a href="#">company</a></li>
+                      <li><a href="#">solutions</a></li>
+                      <li><a href="#">why us</a></li>
+                      <li><a href="#">what we do</a></li>
+                      <li><a href="#">gallery</a></li>
+                      <li><a href="#">awards</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="index-2.html">what we do</a></li>
+                  <li><a href="index-3.html">blog</a></li>
+                  <li><a href="index-4.html">contact</a></li>
+                </ul>
+            </nav>
+          </div>
+      </div> 
+</header>
+<!--==============================content=================================-->
+<section id="content">
+    <div class="row_14">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+           <h2 class="h2-pad">Search result:</h2>
+            <div id="search-results"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-push-6 col-md-push-6 col-sm-push-3 col-lg-6 col-md-6 col-sm-9 colfooter">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 colfooter2">
+                        <p class="m_top1">8901 Marmora Road,<br>Glasgow, D04 89GR.</p>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 colfooter2">
+                        <a class="m_top1" href="#"><i class="icon-envelope-alt"></i>mail@demolink.org</a>
+                        <p class="footerphone"><i class="icon-phone"></i>+1 800 559 6580</p>
+                    </div>
+                </div>   
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-lg-pull-6 col-md-pull-6 col-sm-pull-9 colfooter">
+                <a class="smalllogo" href="index.html"><img alt="dock" src="img/logosmall.png"></a>  
+                <p class="privacytxt">DOCK &bull; <span>&copy; 2013</span></p> 
+                <a class="footerprivlink" href="index-5.html"> Privacy policy</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
